015a23a6dc003974d3da8918910a20a4b34689de
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-02-17 23:47:34 -0500

    added semantic-ui package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import HomeContainer from './containers/HomeContainer'
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" component={HomeContainer}/>
-        <Route exact path="/home" component={HomeContainer} />
-      </Switch>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/" component={HomeContainer}/>
+          <Route exact path="/home" component={HomeContainer} />
+        </Switch>
+      </div>
     );
   }
 }
