0be4a4f99457513bb1fd38528b59a89e892b5f3e
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-03-04 22:46:33 -0500

    centered text

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,13 @@ class About extends React.Component {
       <div>
         <Grid>
           <Grid.Column width={4} />
-          <Grid.Column width={8}>
-            <h1>About</h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <Grid.Column width={8} textAlign='center'>
+            <Grid.Row>
+              <h1>About</h1>
+            </Grid.Row>
+            <Grid.Row>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Grid.Row>
           </Grid.Column>
           <Grid.Column width={4} />
         </Grid>
