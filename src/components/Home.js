ae28abf3d02d248d74b9c078d2d99d2436488828
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-03-11 20:46:19 -0400

    added images and url-loader package

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,20 @@ import React from 'react'
 class Home extends React.Component {
 
   render(){
+    const img0 = require('./images/IMG_6197.jpg');
+    const img1 = require('./images/IMG_6198.jpg');
+    const img2 = require('./images/IMG_6199.jpg');
+    const img3 = require('./images/IMG_6200.jpg');
+    const img4 = require('./images/IMG_6201.jpg');
+    const img5 = require('./images/IMG_6202.jpg');
+    const img6 = require('./images/IMG_6202.jpg');
+
+    const imgList = [img0, img1, img2, img3, img4, img5, img6];
+
     return (
       <div>
         <h2>Home</h2>
-        
+
       </div>
     )
   }
