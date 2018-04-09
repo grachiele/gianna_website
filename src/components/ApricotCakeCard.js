bdf67e7aab046399c327ac59642e7cd1888ffebc
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-04-08 23:30:54 -0400

    simplified code on Home page

--- src/components/ApricotCakeCard.js
+++ src/components/ApricotCakeCard.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Button, Card, Image } from 'semantic-ui-react'
 
+
 class ApricotCard extends React.Component {
-    constructor(){
-      super();
+
+    constructor(props){
+      super(props);
+
+      this.onClickBack = this.onClickBack.bind(this)
+      this.onClickForward = this.onClickForward.bind(this)
 
       const img0 = require('./images/apricot/IMG_6197.jpg');
       const img1 = require('./images/apricot/IMG_6198.jpg');
@@ -17,23 +22,51 @@ class ApricotCard extends React.Component {
         index: 0,
         imgList: [img0, img1, img2, img3, img4, img5, img6]
       }
+
     }
 
+    onClickForward() {
+      if (this.state.index + 1 === this.state.imgList.length) {
+        this.setState({
+          index: 0
+        })
+      } else {
+        this.setState({
+          index: this.state.index + 1
+        })
+      }
+    }
+
+    onClickBack() {
+      if (this.state.index - 1 === -1) {
+        this.setState({
+          index: this.state.imgList.length - 1
+        })
+      } else {
+        this.setState({
+          index: this.state.index - 1
+        })
+      }
+    }
+
+
     render(){
-      <Card>
-        <div className="imgContainer">
-            <Image src={this.state.imgList[this.state.index]} size='medium' verticalAlign="middle"/>
-        </div>
-        <Card.Content>
-          <Card.Header>Naked Apricot Cake</Card.Header>
-          <Card.Meta>Apricot with a frosting of sorts</Card.Meta>
-          <Card.Description>Naked layered cake</Card.Description>
-        </Card.Content>
-        <Card.Content extra textAlign='center'>
-          <Button onClick={this.onClickBack} content='Back' icon='left arrow' labelPosition='left' />
-          <Button onClick={this.onClickForward} content='Next' icon='right arrow' labelPosition='right' />
-        </Card.Content>
-      </Card>
+      return (
+        <Card>
+          <div className="imgContainer">
+              <Image src={this.state.imgList[this.state.index]} size='medium' verticalAlign="middle"/>
+          </div>
+          <Card.Content>
+            <Card.Header>Naked Apricot Cake</Card.Header>
+            <Card.Meta>Apricot with a frosting of sorts</Card.Meta>
+            <Card.Description>Naked layered cake</Card.Description>
+          </Card.Content>
+          <Card.Content extra textAlign='center'>
+            <Button onClick={this.onClickBack} content='Back' icon='left arrow' labelPosition='left' />
+            <Button onClick={this.onClickForward} content='Next' icon='right arrow' labelPosition='right' />
+          </Card.Content>
+        </Card>
+      )
     }
 }
 
